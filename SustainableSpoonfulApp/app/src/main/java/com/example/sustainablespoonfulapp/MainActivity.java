55851e9c0347525b3f52de98703b5109d557db93
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-06-16 20:12:05 +0200

    Added On Click Events for the Login and Register Buttons on the home page.

--- SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/MainActivity.java
+++ SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sustainablespoonfulapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*Navigating to the Registration Page After Clicking the Register Button*/
+        Button registerButton=findViewById(R.id.home_register_button);
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*Navigating to the Login Page After Clicking the Login Button*/
+        Button loginButton=findViewById(R.id.home_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
--- SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/RegisterActivity.java
+++ SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.sustainablespoonfulapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
