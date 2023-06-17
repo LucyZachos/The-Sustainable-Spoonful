097911838bf4b412bc2683044202458f22346d3b
MelanyOpperman <melanyopp@outlook.com>
2023-06-17 15:30:59 +0200

    Changed the style and added the borders to register.

--- SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/DiscountActivity.java
+++ SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/DiscountActivity.java
@@ -1,4 +1,14 @@
 package com.example.sustainablespoonfulapp;
 
-public class DiscountActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiscountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discount);
+    }
 }
