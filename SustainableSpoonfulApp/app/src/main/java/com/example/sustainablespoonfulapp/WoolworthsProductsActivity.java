95a5032b0df847a0ca40b57a3a36845a702d1c52
MelanyOpperman <melanyopp@outlook.com>
2023-06-18 20:39:38 +0200

    Created the Pick n Pay products page, as well as the Woolworths products page; added the nav bar to each of those pages too.

--- SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/WoolworthsProductsActivity.java
+++ SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/WoolworthsProductsActivity.java
@@ -1,5 +1,7 @@
 package com.example.sustainablespoonfulapp;
 
+import android.view.View;
+import android.widget.Toast;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,19 +15,19 @@ import androidx.cardview.widget.CardView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class DiscountActivity extends AppCompatActivity {
+
+public class WoolworthsProductsActivity extends AppCompatActivity {
 
     BottomNavigationView bottom_nav_bar;
 
-    CardView cardPicknpay;
-    CardView cardCheckers;
-    CardView cardWoolworths;
-    CardView cardFoodLoversMarket;
+    CardView cardMilk;
+    CardView cardBananas;
+    CardView cardApples;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discount);
+        setContentView(R.layout.activity_woolworths_products);
 
         //Checking if the customer is logged in:
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
@@ -33,8 +35,8 @@ public class DiscountActivity extends AppCompatActivity {
 
         //If the customer is not logged in, display a message and redirect to the main page when opening the application:
         if(email.isEmpty()){
-            Toast.makeText(DiscountActivity.this, "Please log in to continue!", Toast.LENGTH_SHORT).show(); //Display a message to the customer asking them to log in:
-            startActivity(new Intent(DiscountActivity.this, MainActivity.class)); //Redirect the customer to the main page when opening the application:
+            Toast.makeText(WoolworthsProductsActivity.this, "Please log in to continue!", Toast.LENGTH_SHORT).show(); //Display a message to the customer asking them to log in:
+            startActivity(new Intent(WoolworthsProductsActivity.this, MainActivity.class)); //Redirect the customer to the main page when opening the application:
             finish(); //Finishing the current activity so that customers' cannot go back to it when pressing the back button:
             return; //Return early so that the rest of the method is not executed:
         }
@@ -49,17 +51,17 @@ public class DiscountActivity extends AppCompatActivity {
                 switch (item.getItemId()){
                     //If the home icon is clicked, go to the home page:
                     case R.id.home_bottom_navigation:
-                        startActivity(new Intent(DiscountActivity.this,LandingActivity.class)); //Redirect the customer to the home page:
+                        startActivity(new Intent(WoolworthsProductsActivity.this,LandingActivity.class)); //Redirect the customer to the home page:
                         finish(); //Finishing the current activity so that customers' cannot go back to it when pressing the back button:
                         break;
-                        //If the search icon is clicked,stay on the search discounts page:
+                    //If the search icon is clicked,stay on the search discounts page:
                     case R.id.search_bottom_navigation:
-                        startActivity(new Intent(DiscountActivity.this,DiscountActivity.class)); //Stay on the search discount page:
+                        startActivity(new Intent(WoolworthsProductsActivity.this,DiscountActivity.class)); //Stay on the search discount page:
                         finish(); //Finishing the current activity so that customers' cannot go back to it when pressing the back button:
                         break;
-                        //If the account icon is clicked, go to the account page:
+                    //If the account icon is clicked, go to the account page:
                     case R.id.account_bottom_navigation:
-                        startActivity(new Intent(DiscountActivity.this,AccountActivity.class)); //Redirect the customer to the account page:
+                        startActivity(new Intent(WoolworthsProductsActivity.this,AccountActivity.class)); //Redirect the customer to the account page:
                         finish(); //Finishing the current activity so that customers' cannot go back to it when pressing the back button:
                         break;
                     default:
@@ -69,38 +71,32 @@ public class DiscountActivity extends AppCompatActivity {
         });
 
         //For the cards on the layout screen
-        cardPicknpay = findViewById(R.id.cardPicknpay);
-        cardCheckers = findViewById(R.id.cardCheckers);
-        cardWoolworths = findViewById(R.id.cardWoolworths);
-        cardFoodLoversMarket = findViewById(R.id.cardFoodLoversMarket);
+        cardMilk = findViewById(R.id.cardMilk);
+        cardBananas = findViewById(R.id.cardBananas);
+        cardApples = findViewById(R.id.cardApples);
 
-        cardPicknpay.setOnClickListener(new View.OnClickListener() {
+
+        cardMilk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showToast("Pick n Pay Clicked");
+                showToast("Bread Clicked");
             }
         });
-        cardCheckers .setOnClickListener(new View.OnClickListener() {
+        cardBananas .setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showToast("Checkers Clicked");
+                showToast("Oranges Clicked");
             }
         });
-        cardWoolworths .setOnClickListener(new View.OnClickListener() {
+        cardApples .setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showToast("Woolworths Clicked");
-            }
-        });
-        cardFoodLoversMarket.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast("Food Lovers Market Clicked");
+                showToast("Oil Clicked");
             }
         });
     }
 
     private void showToast(String message) {
-        Toast.makeText(DiscountActivity.this, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(WoolworthsProductsActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 }
