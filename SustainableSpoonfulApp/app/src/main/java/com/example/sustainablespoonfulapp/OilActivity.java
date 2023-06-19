646bcb80e604bce1f159227f16312fffa613bc94
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-06-19 18:55:43 +0200

    Added missing brackets to activity files.

--- SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/OilActivity.java
+++ SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/OilActivity.java
@@ -10,4 +10,5 @@ public class OilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oil);
+    }
 }
--- SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/OrangesActivity.java
+++ SustainableSpoonfulApp/app/src/main/java/com/example/sustainablespoonfulapp/OrangesActivity.java
@@ -10,4 +10,5 @@ public class OrangesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oranges);
+    }
 }
